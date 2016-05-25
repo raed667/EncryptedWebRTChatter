d7f379032c0aac7829a84ecc8b4a165e81283ebb
Raed <raed@riseup.net>
2016-05-25 22:31:15 +0200

    Style

--- js/adapter.js
+++ js/adapter.js
@@ -5,85 +5,85 @@ var reattachMediaStream = null
 var webrtcDetectedBrowser = null
 
 if (navigator.mozGetUserMedia) {
-  console.log('This appears to be Firefox')
+    console.log('This appears to be Firefox')
 
-  webrtcDetectedBrowser = 'firefox'
+    webrtcDetectedBrowser = 'firefox'
 
-  // The RTCPeerConnection object.
-  RTCPeerConnection = mozRTCPeerConnection
+    // The RTCPeerConnection object.
+    RTCPeerConnection = mozRTCPeerConnection
 
-  // The RTCSessionDescription object.
-  RTCSessionDescription = mozRTCSessionDescription
+    // The RTCSessionDescription object.
+    RTCSessionDescription = mozRTCSessionDescription
 
-  // The RTCIceCandidate object.
-  RTCIceCandidate = mozRTCIceCandidate
+    // The RTCIceCandidate object.
+    RTCIceCandidate = mozRTCIceCandidate
 
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.mozGetUserMedia.bind(navigator)
+    // Get UserMedia (only difference is the prefix).
+    // Code from Adam Barth.
+    getUserMedia = navigator.mozGetUserMedia.bind(navigator)
 
-  // Attach a media stream to an element.
-  attachMediaStream = function (element, stream) {
-    console.log('Attaching media stream')
-    element.mozSrcObject = stream
-    element.play()
-  }
+    // Attach a media stream to an element.
+    attachMediaStream = function (element, stream) {
+        console.log('Attaching media stream')
+        element.mozSrcObject = stream
+        element.play()
+    }
 
-  reattachMediaStream = function (to, from) {
-    console.log('Reattaching media stream')
-    to.mozSrcObject = from.mozSrcObject
-    to.play()
-  }
+    reattachMediaStream = function (to, from) {
+        console.log('Reattaching media stream')
+        to.mozSrcObject = from.mozSrcObject
+        to.play()
+    }
 
-  // Fake get{Video,Audio}Tracks
-  MediaStream.prototype.getVideoTracks = function () {
-    return []
-  }
+    // Fake get{Video,Audio}Tracks
+    MediaStream.prototype.getVideoTracks = function () {
+        return []
+    }
 
-  MediaStream.prototype.getAudioTracks = function () {
-    return []
-  }
+    MediaStream.prototype.getAudioTracks = function () {
+        return []
+    }
 } else if (navigator.webkitGetUserMedia) {
-  console.log('This appears to be Chrome')
+    console.log('This appears to be Chrome')
 
-  webrtcDetectedBrowser = 'chrome'
+    webrtcDetectedBrowser = 'chrome'
 
-  // The RTCPeerConnection object.
-  RTCPeerConnection = webkitRTCPeerConnection
+    // The RTCPeerConnection object.
+    RTCPeerConnection = webkitRTCPeerConnection
 
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.webkitGetUserMedia.bind(navigator)
+    // Get UserMedia (only difference is the prefix).
+    // Code from Adam Barth.
+    getUserMedia = navigator.webkitGetUserMedia.bind(navigator)
 
-  // Attach a media stream to an element.
-  attachMediaStream = function (element, stream) {
-    element.src = webkitURL.createObjectURL(stream)
-  }
-
-  reattachMediaStream = function (to, from) {
-    to.src = from.src
-  }
-
-  // The representation of tracks in a stream is changed in M26.
-  // Unify them for earlier Chrome versions in the coexisting period.
-  if (!webkitMediaStream.prototype.getVideoTracks) {
-    webkitMediaStream.prototype.getVideoTracks = function () {
-      return this.videoTracks
+    // Attach a media stream to an element.
+    attachMediaStream = function (element, stream) {
+        element.src = webkitURL.createObjectURL(stream)
     }
-    webkitMediaStream.prototype.getAudioTracks = function () {
-      return this.audioTracks
-    }
-  }
 
-  // New syntax of getXXXStreams method in M26.
-  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
-    webkitRTCPeerConnection.prototype.getLocalStreams = function () {
-      return this.localStreams
+    reattachMediaStream = function (to, from) {
+        to.src = from.src
     }
-    webkitRTCPeerConnection.prototype.getRemoteStreams = function () {
-      return this.remoteStreams
+
+    // The representation of tracks in a stream is changed in M26.
+    // Unify them for earlier Chrome versions in the coexisting period.
+    if (!webkitMediaStream.prototype.getVideoTracks) {
+        webkitMediaStream.prototype.getVideoTracks = function () {
+            return this.videoTracks
+        }
+        webkitMediaStream.prototype.getAudioTracks = function () {
+            return this.audioTracks
+        }
     }
-  }
+
+    // New syntax of getXXXStreams method in M26.
+    if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
+        webkitRTCPeerConnection.prototype.getLocalStreams = function () {
+            return this.localStreams
+        }
+        webkitRTCPeerConnection.prototype.getRemoteStreams = function () {
+            return this.remoteStreams
+        }
+    }
 } else {
-  console.log('Browser does not appear to be WebRTC-capable')
+    console.log('Browser does not appear to be WebRTC-capable')
 }
